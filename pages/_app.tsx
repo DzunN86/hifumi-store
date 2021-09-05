3388c2b02de440bd67428137753e97a7169f9f69
Dzun <nurroinz@gmail.com>
2021-09-05 23:38:55 +0700

    slicing landing page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/utilities.css';
+import '../styles/homepage.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        {/* Bootstrap Css */}
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x"
+          crossOrigin="anonymous"
+        />
+        {/* Bootstrap JS */}
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+          crossOrigin="anonymous"
+        />
+        {/* Google Font */}
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        {/* AOS Animation */}
+        <link
+          href="https://unpkg.com/aos@2.3.1/dist/aos.css"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp
+export default MyApp;
